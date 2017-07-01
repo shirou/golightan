fb7c99bdba515ff38df5de97d75f78b1eaa1c688
WAKAYAMA Shirou <shirou.faw@gmail.com>
2017-07-01 10:56:17 +0900

    add show available lexers and formatters.

--- cmd/highlighter/main.go
+++ cmd/highlighter/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/shirou/highlighter/lexer"
 )
@@ -20,14 +22,21 @@ func main() {
 	var formatter string
 	var style string
 	var out string
+	var list bool
 
 	flag.StringVar(&lexer, "l", "", "lexer")
 	flag.StringVar(&formatter, "f", "html", "formatter")
 	flag.StringVar(&style, "s", "default", "html style")
 	flag.StringVar(&filter, "F", "", "filters")
+	flag.BoolVar(&list, "L", false, "list available lexers and formatters")
 	flag.StringVar(&out, "o", "-", "output")
 
 	flag.Parse()
+
+	if list {
+		showList()
+		return
+	}
 
 	var output io.Writer
 	var err error
@@ -75,4 +84,24 @@ func guessByExt(filename string) string {
 		}
 	}
 	return ""
+}
+
+func showList() {
+	fmt.Println("# Available Lexers")
+	tmpl := `  * %s (%s)
+      %s
+`
+	for _, p := range lexer.AvailableLexers() {
+		fmt.Printf(tmpl,
+			strings.Join(p.Targets, ", "),
+			strings.Join(p.Exts, ", "),
+			p.Description)
+	}
+	fmt.Println("")
+	fmt.Println("# Available Formatters")
+	f := []string{"html (default)", "terminal", "raw"}
+	for _, p := range f {
+		fmt.Printf("  * %s\n", p)
+	}
+
 }
